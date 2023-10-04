7bcd473155fd25d483df5389cda1b4251aaf7a95
JessicaKalousek <jkalousek@itba.edu.ar>
2023-10-04 16:20:43 -0300

    corregir path

--- src/workflow-experimentos/Experimento3/y601_RUN_correr_workflow.r
+++ src/workflow-experimentos/Experimento3/y601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-experimentos/y611_CA_reparar_dataset.r")
-source("~/dm2023b/src/workflow-experimentos/y621_DR_corregir_drifting.r")
-source("~/dm2023b/src/workflow-experimentos/y631_FE_historia.r")
-source("~/dm2023b/src/workflow-experimentos/y641_TS_training_strategy.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y611_CA_reparar_dataset.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y621_DR_corregir_drifting.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y631_FE_historia.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-experimentos/y651_HT_lightgbm.r")
-source("~/dm2023b/src/workflow-experimentos/y661_ZZ_final.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y651_HT_lightgbm.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento3/y661_ZZ_final.r")
