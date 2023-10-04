7235c6ae91d40f0b971e1e901c834da4a5777836
JessicaKalousek <jkalousek@itba.edu.ar>
2023-10-04 00:25:02 -0300

    corregir exp

--- src/workflow-experimentos/Experimento1/e601_RUN_correr_workflow.r
+++ src/workflow-experimentos/Experimento1/e601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-experimentos/e611_CA_reparar_dataset.r")
-source("~/dm2023b/src/workflow-experimentos/e621_DR_corregir_drifting.r")
-source("~/dm2023b/src/workflow-experimentos/e631_FE_historia.r")
-source("~/dm2023b/src/workflow-experimentos/e641_TS_training_strategy.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e611_CA_reparar_dataset.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e621_DR_corregir_drifting.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e631_FE_historia.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-experimentos/e651_HT_lightgbm.r")
-source("~/dm2023b/src/workflow-experimentos/e661_ZZ_final.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e651_HT_lightgbm.r")
+source("~/dm2023b/src/workflow-experimentos/Experimento1/e661_ZZ_final.r")
--- src/workflow-experimentos/ExperimentoWFInicial/w601_RUN_correr_workflow.r
+++ src/workflow-experimentos/ExperimentoWFInicial/w601_RUN_correr_workflow.r
@@ -8,13 +8,14 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
+#\dm2023b\src\workflow-experimentos\ExperimentoWFInicial
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-inicial/w611_CA_reparar_dataset.r")
-source("~/dm2023b/src/workflow-inicial/w621_DR_corregir_drifting.r")
-source("~/dm2023b/src/workflow-inicial/w631_FE_historia.r")
-source("~/dm2023b/src/workflow-inicial/w641_TS_training_strategy.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w611_CA_reparar_dataset.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w621_DR_corregir_drifting.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w631_FE_historia.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-inicial/w651_HT_lightgbm.r")
-source("~/dm2023b/src/workflow-inicial/w661_ZZ_final.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w651_HT_lightgbm.r")
+source("~/dm2023b/src/workflow-experimentos/ExperimentoWFInicial/w661_ZZ_final.r")
